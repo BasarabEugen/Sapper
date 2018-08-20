da0285f1d308e702a3efe5b08e0e9633f31cfb33
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-08-20 20:40:13 +0300

    init panel

--- src/JavaSapper.java
+++ src/JavaSapper.java
@@ -1,2 +1,31 @@
-public class JavaSapper {
+import javax.swing.*;
+import java.awt.*;
+
+public class JavaSapper extends JFrame {
+    private JPanel panel;
+
+
+    public static void main(String[] args) {
+        new JavaSapper();
+    }
+
+    private JavaSapper() {
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sapper");
+        setVisible(true);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
